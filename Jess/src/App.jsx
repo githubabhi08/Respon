d17abe3ee1dd05d86646d0404bf4c70d3351d18e
Abhi <dgonedas@gmail.com>
2024-05-09 21:51:15 +0530

    vites

---
+++ Jess/src/App.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './App.css'
+function App() {
+  return (
+    <>
+    
+    
+      <div className='cola'>
+      <ul className='navba'>
+        <li className='item'>HOME</li>
+        <li className='item'>PRODUCTS</li>
+        <li className='item'>CATEGEORY</li>
+        <li className='item'>ABOUT</li>
+        <li className='item'>CONTACT</li>
+
+      </ul>
+      
+      </div>
+  
+
+     
+    
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default App
+
+
+
+
---
+++ Jess/src/main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+<script src="https://kit.fontawesome.com/c40473c49b.js" crossorigin="anonymous"></script>
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
